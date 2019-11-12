e2197aac8d409243f91bf03137d8d6619eed10e6
Konrad Chojnacki <konrad.cho@gmail.com>
2019-11-12 15:01:42 +0100

    Fill Screen - implementation

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,62 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(WHILE)
+    @8191 // screen's "length" in 16bit-words = 8192 -> 0 - 8191
+    D=A
+
+    @n  
+    M=D
+
+    @KBD
+    D=M
+
+    @color
+    M=0
+
+    @DRAW
+    D;JEQ
+
+    @color
+    M=-1 // RAM[addr]=1111111111111111
+
+(DRAW)
+
+    @SCREEN
+    D=A
+    
+    @addr
+    M=D // addr = 16384
+        // (screen’s base address)
+    @i
+    M=0 //i=0
+
+(LOOP)
+    @i
+    D=M
+
+    @n
+    D=D-M
+
+    @END
+    D;JGT // if i>n goto END
+
+    @color
+    D=M
+
+    @addr
+    A=M
+    M=D 
+
+    @i
+    M=M+1 //i=i+1
+
+    @addr
+    M=M+1 
+
+    @LOOP
+    0;JMP
+
+(END)
+    @WHILE
+    0;JMP
