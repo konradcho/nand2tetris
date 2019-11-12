9273af6d071d0a6157b02ffceaf78b36aebfb66e
Konrad Chojnacki <konrad.cho@gmail.com>
2019-11-12 14:02:06 +0100

    Multiplication implementation

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,34 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R0
+D=M
+
+@i
+M=D
+
+@R2
+M=0
+
+(LOOP)
+    @i
+    D=M
+
+    @END
+    D;JLE
+
+    @R1
+    D=M
+
+    @R2
+    M=M+D
+
+    @i
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
